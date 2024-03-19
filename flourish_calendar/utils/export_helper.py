26de11f2f1414c28e889e70df23ea2db62a4c6bd
nmunatsibw <mcturnergarai@gmail.com>
2024-03-19 15:02:40 +0200

    refactor(Export): Add age to to the calendar export

--- flourish_calendar/utils/export_helper.py
+++ flourish_calendar/utils/export_helper.py
@@ -1,7 +1,6 @@
 import datetime
 
 from django.apps import apps as django_app
-from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 from edc_base.utils import age, get_utcnow
 
@@ -43,40 +42,13 @@ def extract_cohort_name(subject_identifier):
     except cohort_model_cls.DoesNotExist:
         return None
     else:
-        return cohort.name
+        return cohort
 
 
-def cohort_objs(subject_identifier):
-    cohort_model = 'flourish_caregiver.cohort'
-    cohort_model_cls = django_app.get_model(cohort_model)
-    return cohort_model_cls.objects.filter(
-        subject_identifier=subject_identifier)
-
-
-def enrolment_cohort(subject_identifier):
-    try:
-        return cohort_objs(subject_identifier).filter(
-            enrollment_cohort=True).latest('assign_datetime')
-    except ObjectDoesNotExist:
-        return None
-
-
-def current_cohort(subject_identifier):
-    try:
-        return cohort_objs(subject_identifier).filter(
-            current_cohort=True).latest('assign_datetime')
-    except ObjectDoesNotExist:
-        return None
-
-
-def get_child_age(subject_identifier=None):
-    caregiver_child_consent_model = 'flourish_caregiver.caregiverchildconsent'
-    caregiver_child_consent_cls = django_app.get_model(caregiver_child_consent_model)
-    try:
-        consent = caregiver_child_consent_cls.objects.filter(
-            subject_identifier=subject_identifier).latest('consent_datetime')
-    except caregiver_child_consent_cls.DoesNotExist:
-        return None
-    else:
-        _age = age(consent.child_dob, get_utcnow())
-        return _age.years + _age.months / 12
+def participant_age(subject_identifier):
+    cohort_obj = extract_cohort_name(subject_identifier)
+    if cohort_obj:
+        caregiver_child_consent_obj = cohort_obj.caregiver_child_consent
+        if caregiver_child_consent_obj:
+            child_age = age(caregiver_child_consent_obj.child_dob, get_utcnow())
+            return round(child_age.years + child_age.months/12, 1)
